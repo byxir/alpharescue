3f66ec797854801cdfb9d236e546cbc9cf089bc5
Egor Solodovnikov <egor.solo1@yandex.ru>
2023-04-26 19:43:43 +0200

    please

--- src/pages/api/whop/createRafflebotSubscription.ts
+++ src/pages/api/whop/createRafflebotSubscription.ts
@@ -13,37 +13,36 @@ const userByIdHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   if (!id || typeof id != "string") {
-    return (
-      res
-        .status(200)
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        .json({ error: "connection established", message: id })
-    );
+    return res.status(200).json({ error: "connection established" });
   } else {
     const currentUser = await prisma.account.findFirst({
       where: {
         providerAccountId: id,
       },
       include: {
-        user: true,
-      },
-    });
-
-    const newFlags = await prisma.account.update({
-      where: {
-        id: currentUser?.id,
-      },
-      data: {
         user: {
-          update: {
-            raffleBotUser: true,
-            communityMember: true,
+          include: {
+            RaffleBotSubscription: true,
           },
         },
       },
     });
 
-    if (currentUser) {
+    if (currentUser && !currentUser.user.RaffleBotSubscription) {
+      const newFlags = await prisma.account.update({
+        where: {
+          id: currentUser?.id,
+        },
+        data: {
+          user: {
+            update: {
+              raffleBotUser: true,
+              communityMember: true,
+            },
+          },
+        },
+      });
+
       const newSubscription = await prisma.raffleBotSubscription.create({
         data: {
           expires: new Date(),
@@ -61,8 +60,6 @@ const userByIdHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.status(200).json({
       message: "good",
-      currentUser: currentUser,
-      newFlags,
     });
   }
 };
