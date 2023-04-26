2390b403441af91c37c835b7527b726a429072b5
Egor Solodovnikov <egor.solo1@yandex.ru>
2023-04-26 19:27:50 +0200

    please

--- src/pages/api/whop/createRafflebotSubscription.ts
+++ src/pages/api/whop/createRafflebotSubscription.ts
@@ -1,70 +1,70 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { prisma } from "../../../server/db";
 
-const userByIdHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "GET") return res.status(200).end();
+const userByIdHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  return res.status(200);
 
-  let id;
+  // let id;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  if (req.body.data.user.social_accounts[0].id) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    id = req.body.data.user.social_accounts[0].id;
-  }
+  // // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  // if (req.body.data.user.social_accounts[0].id) {
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  //   id = req.body.data.user.social_accounts[0].id;
+  // }
 
-  if (!id || typeof id != "string") {
-    return (
-      res
-        .status(200)
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        .json({ error: "connection established", message: id })
-    );
-  } else {
-    const currentUser = await prisma.account.findFirst({
-      where: {
-        providerAccountId: id,
-      },
-      include: {
-        user: true,
-      },
-    });
+  // if (!id || typeof id != "string") {
+  //   return (
+  //     res
+  //       .status(200)
+  //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  //       .json({ error: "connection established", message: id })
+  //   );
+  // } else {
+  //   const currentUser = await prisma.account.findFirst({
+  //     where: {
+  //       providerAccountId: id,
+  //     },
+  //     include: {
+  //       user: true,
+  //     },
+  //   });
 
-    const newFlags = await prisma.account.update({
-      where: {
-        id: currentUser?.id,
-      },
-      data: {
-        user: {
-          update: {
-            raffleBotUser: true,
-            communityMember: true,
-          },
-        },
-      },
-    });
+  //   const newFlags = await prisma.account.update({
+  //     where: {
+  //       id: currentUser?.id,
+  //     },
+  //     data: {
+  //       user: {
+  //         update: {
+  //           raffleBotUser: true,
+  //           communityMember: true,
+  //         },
+  //       },
+  //     },
+  //   });
 
-    if (currentUser) {
-      const newSubscription = await prisma.raffleBotSubscription.create({
-        data: {
-          expires: new Date(),
-          rafflesLeft: 5,
-          rafflesPerDay: 5,
-          maxNumAccounts: 150,
-          user: {
-            connect: {
-              id: currentUser?.userId,
-            },
-          },
-        },
-      });
-    }
+  //   if (currentUser) {
+  //     const newSubscription = await prisma.raffleBotSubscription.create({
+  //       data: {
+  //         expires: new Date(),
+  //         rafflesLeft: 5,
+  //         rafflesPerDay: 5,
+  //         maxNumAccounts: 150,
+  //         user: {
+  //           connect: {
+  //             id: currentUser?.userId,
+  //           },
+  //         },
+  //       },
+  //     });
+  //   }
 
-    res.status(200).json({
-      message: "good",
-      currentUser: currentUser,
-      newFlags,
-    });
-  }
+  //   res.status(200).json({
+  //     message: "good",
+  //     currentUser: currentUser,
+  //     newFlags,
+  //   });
+  // }
 };
 
 export default userByIdHandler;
